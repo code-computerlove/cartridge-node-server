c03f3a513504fd74eff3ab9421963dcdd8f6acd4
Barney Scott <barney.scott@codecomputerlove.com>
2016-04-29 16:22:14 +0100

    style(models): Updated form-model to pass the linter
    Updated the form model to pass the linter and enabled linting again in travis now sections of the
    code are passing

--- models/form-model.js
+++ models/form-model.js
@@ -1,22 +1,17 @@
 (function(){
-  'use strict';
+	'use strict';
 
-  var rawData = null;
+	var FormModel = function(data) {
+		return {
+			email : data.email,
+			firstName: data.firstname,
+			lastName: data.lastname,
+			phone : data.phone,
+			organisation : data.organisation,
+			website: data.website
+		};
+	};
 
-  var FormModel = function(data) {
-
-    rawData = data;
-
-    return {
-      email : data.email,
-      firstName: data.firstname,
-      lastName: data.lastname,
-      phone : data.phone,
-      organisation : data.organisation,
-      website: data.website
-    };
-  };
-
-  module.exports = FormModel;
+	module.exports = FormModel;
 
 }());

b33819ae9fd9b24fc19f93d64991273ee0d65aa9
tawashley <trisashley@gmail.com>
2016-10-12 13:36:08 +0100

    feat: Add back in server error codez

--- router.js
+++ router.js
@@ -8,11 +8,12 @@ module.exports = function(app) {
 	var homeController = require('./controllers/home');
 	var styleguideController = require('./controllers/styleguide');
 	var pageNotFoundController = require('./controllers/page-not-found');
+	var errorController = require('./controllers/server-error');
 
 	app.get('/page-not-found', pageNotFoundController.get);
+	app.get('/error', errorController.get);
 	app.get('/styleguide', styleguideController.get);
 	app.get('/', homeController.get);
-
 
 	app.use(function(request, response) {
 		response.redirect('/page-not-found');

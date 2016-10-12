c26e8c7ce25ee098bae270b2a1f9cea18ffff926
tawashley <trisashley@gmail.com>
2016-10-12 12:43:02 +0100

    chore: Remove uneeded controller code

--- router.js
+++ router.js
@@ -6,15 +6,11 @@ var upload = multer({dest: 'uploads'});
 module.exports = function(app) {
 
 	var homeController = require('./controllers/home');
-	var sitemapController = require('./controllers/sitemap');
 	var styleguideController = require('./controllers/styleguide');
 	var pageNotFoundController = require('./controllers/page-not-found');
-	var errorController = require('./controllers/server-error');
 
 	app.get('/page-not-found', pageNotFoundController.get);
-	app.get('/error', errorController.get);
 	app.get('/styleguide', styleguideController.get);
-	app.get('/sitemap.xml', sitemapController.get);
 	app.get('/', homeController.get);
 
 
